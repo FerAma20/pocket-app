7e9d2015c43ee2463885fa26916846a40b5720ff
fescobar <fescobar@bytesw.com>
2025-11-16 23:08:25 -0600

    Correccion configuracion theme

--- app/src/main/java/com/example/mypocketapp/MainActivity.kt
+++ app/src/main/java/com/example/mypocketapp/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
+            MyPocketAppTheme {
                 val navController = rememberNavController()
                 AppNavHost(navController)
             }
